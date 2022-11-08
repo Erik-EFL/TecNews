31e3a8b47ce39f561dbe686f45b12692d864f21a
erik-efl <erik.lima.f@outlook.com>
2022-11-08 19:58:36 -0300

    feat: :sparkles: Adding new function
    Implementing function search by title

--- tech_news/analyzer/search_engine.py
+++ tech_news/analyzer/search_engine.py
@@ -1,11 +1,17 @@
+from tech_news.database import search_news
+# from datetime import datetime
+
+
 # Requisito 6
 def search_by_title(title):
-    """Seu código deve vir aqui"""
+    response = search_news({"title": {"$regex": title, "$options": "i"}})
+    return [(news["title"], news["url"]) for news in response]
 
 
 # Requisito 7
 def search_by_date(date):
     """Seu código deve vir aqui"""
+
 
 
 # Requisito 8
