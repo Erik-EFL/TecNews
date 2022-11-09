285f609391509a2c96d6a70f896855bd6cc1d4b0
erik-efl <erik.lima.f@outlook.com>
2022-11-09 16:36:00 -0300

    feat: :sparkles: Adding a new function
    Implementing function top five news

--- tech_news/analyzer/ratings.py
+++ tech_news/analyzer/ratings.py
@@ -1,6 +1,13 @@
+from tech_news.database import find_news
+
+
 # Requisito 10
 def top_5_news():
-    """Seu código deve vir aqui"""
+    news_list = find_news()
+    better_rating = sorted(
+        news_list, key=lambda news: (-news["comments_count"], news["title"]))
+
+    return [(news["title"], news["url"]) for news in better_rating[:5]]
 
 
 # Requisito 11
